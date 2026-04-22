98eafc16682a6e641ea7b20ace4a2a3abc5e93bf
Tomiloba2 <oluwatomilobaomojola@gmail.com>
2026-04-22 09:36:04 -0700

    feat:initialize prisma,express,typescript and postgres

---
+++ index.ts
@@ -0,0 +1,29 @@
+import "dotenv/config"
+import express, { Application, NextFunction, Response, Request } from "express"
+import cors from "cors"
+
+
+const app: Application = express()
+
+app.use(cors({
+    origin: "*"
+}))
+
+app.use(express.json())
+app.use(express.urlencoded({
+    extended: true
+}))
+
+
+/* errorHandler middleware */
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+    const status = err.statusCode ? err.statusCode : 500
+    return res.status(status).json({
+        status: "error",
+        message: err.message
+    })
+})
+
+app.listen(3000, () => {
+    console.log("server is live");
+})
